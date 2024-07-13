1be613206c774c4f2ad5fa1c1143a26252020872
Irec <irec.other@gmail.com>
2024-07-13 14:39:06 -0700

    Added default Unreal Engine 5 C++ setup, an empty base level, and .gitignore file.

---
+++ GridBasedRTS/Source/GridBasedRTS/GridBasedRTSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GridBasedRTSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDBASEDRTS_API AGridBasedRTSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
